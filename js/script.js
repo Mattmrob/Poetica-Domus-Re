28380d5990a3a54c3b8695c4dbb670ed7f65252e
Mysmelf <matthewr9937@gmail.com>
2023-09-25 16:56:10 -0700

    One more bug fix to wiki link targeting

--- js/script.js
+++ js/script.js
@@ -221,8 +221,8 @@ function wikiApiCall(url) {
         .then(function (data) {
 
             let wikiLink = $('<a href="" class="col-12 wikiLink" target="_blank">More Info on this Artist</a>');
-            $("#wikiLinkTarget").attr("href", data[3][0]);
             $("#contentButtons").append(wikiLink);
+            $(wikiLink).attr("href", data[3][0]);
 
         })
 
